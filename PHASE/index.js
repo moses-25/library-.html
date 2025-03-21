7df2ee968baff4a7803b318617d2c7e25f2001e1
moses-25 <moses.ochieng1@student.moringaschool.com>
2025-03-21 11:43:03 +0300

    added multiple files

---
+++ PHASE/index.js
@@ -0,0 +1,8 @@
+const about me section=document.getElementById('about me');
+console.log(about me section);
+
+let headingElement=document.createElement('h1');
+headingElement.innerHTML='heading';
+let paragraphElement=document.createElement('p');
+paragraphElement.innerHTML='paragraph';
+
